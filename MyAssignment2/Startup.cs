acb690bea0f6cf79deb8009dc210ceca141b9c25
user <user@MACHINE-222>
2017-07-26 11:53:37 -0700

    Add project files.

---
+++ MyAssignment2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyAssignment2.Startup))]
+namespace MyAssignment2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
